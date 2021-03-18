715c2d0c5d3c0d6cb08608270c5276b1acf14bb3
Daniel Gakwaya <wayadn88@gmail.com>
2021-03-18 08:12:12 +0200

    Episode2 code

---
+++ Ep002/source/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+T add( T a, T b){
+    return a + b;
+}
+
+int main(){
+    std::cout << "Hello from C++ 20 with CMake on Linux/Windows" << std::endl;
+    std::cout << "The sum is : " << add(7,5) << std::endl;
+    return 0;
+}
