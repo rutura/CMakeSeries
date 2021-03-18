2bfaf0c5024450d08261a9e0094098decac42d75
Daniel Gakwaya <wayadn88@gmail.com>
2021-03-18 08:57:40 +0200

    Apply changes from linux

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -9,6 +9,6 @@ T add( T a, T b){
 
 int main(){
     std::cout << "Hello from C++ 20 with CMake on Linux/Windows" << std::endl;
-    std::cout << "The sum is : " << add(7,5) << std::endl;
+    std::cout << "The sum is : " << add(7,3) << std::endl;
     return 0;
 }
