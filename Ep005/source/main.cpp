cf9fc03af44dc528df69e252fce8d80669005fc4
Daniel Gakwaya <wayadn88@gmail.com>
2021-04-02 09:01:55 +0200

    Add code for episode 5

---
+++ Ep005/source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "dog.h"
+#include "operations.h"
+
+int main(){
+
+    Dog dog1("Flitzy");
+    dog1.print_info();
+    
+    return 0;
+}
---
+++ Ep005/source/src/operations.cpp
@@ -0,0 +1,3 @@
+double add(double a, double b){
+    return a + b;
+}
