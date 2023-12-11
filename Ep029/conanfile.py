355b86d0d6763f47b2a9f8b9431008cb4b79e9b0
Daniel Gakwaya <wayadn88@gmail.com>
2023-12-11 15:40:25 +0200

    Strip down the conan file

--- Ep029/conanfile.py
+++ Ep029/conanfile.py
@@ -4,14 +4,9 @@ from conan.tools.cmake import CMakeToolchain
 
 class RoosterRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
+    generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
         self.requires("catch2/3.4.0")
         self.requires("fmt/10.1.1")
         self.requires("sfml/2.6.1")
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.user_presets_path = False
-        tc.generate()
