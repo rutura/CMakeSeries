22ad88d970a2d3db5f66e76426d1e792a355bb5e
Daniel Gakwaya <wayadn88@gmail.com>
2023-12-11 15:19:39 +0200

    Add code for Ep029 - conan for dependency management

---
+++ Ep029/conanfile.py
@@ -0,0 +1,17 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain
+
+
+class RoosterRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        self.requires("catch2/3.4.0")
+        self.requires("fmt/10.1.1")
+        self.requires("sfml/2.6.1")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
